0041258ac030e349df2da85a94b94d8c87b57383
Matheus Barbosa Lopes <matheus_barbosa5@hotmail.com>
2020-11-26 16:16:26 -0400

    Tela de pagina não encontrada

--- src/screens/NotFound/index.js
+++ src/screens/NotFound/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Typography } from "@material-ui/core";
+import { Typography, Box } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 
 const NotFound = () => {
@@ -11,7 +11,7 @@ const NotFound = () => {
   }, []);
 
   return (
-    <>
+    <Box pt={5}>
       <Typography
         style={{ textAlign: "center", paddingTop: 50 }}
         variant="h5"
@@ -26,7 +26,7 @@ const NotFound = () => {
       >
         Você será redirecionado para pagina principal em alguns segundos!
       </Typography>
-    </>
+    </Box>
   );
 };
 
